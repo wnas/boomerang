f476c8bfe3b02b5ece159cd18757217a0eee1904
wnas <wilfred@wnas.nl>
2011-12-15 12:10:10 +0100

    win

---
+++ sogetti/project/js/script.js
@@ -0,0 +1,32 @@
+	$('p').fadeIn(1000,function(){
+			setData();
+			
+		//	fadeInEm();
+		//	var s = document.getElementsByTagName('span');
+		//	console.log(s)
+		//	for (i=0;i<=s.length;i++){
+		//	for ( var i = -1; ++i < s;){
+				//setTimeout($('span').addClass('active'),1500);
+		//	}
+		});
+		var setData = function(){
+			//var st = $('span');
+			$('span').each(function(i){
+				var span = $(this);
+			//	var ot = $(this).text();
+				var t = i++;
+				span.attr('data-t',t);
+				dostuff(span);
+			//	console.log($(this).text())
+				//
+			});
+		},	dostuff = function(span){
+			var time = span.attr('data-t');
+			time = time*100;
+		//	console.log(typeof time);
+		//	console.log(span.attr('data-t'));
+			setTimeout( function(){span.addClass('active');},time);
+			return false;
+			//$(this).next('span').addClass('active').attr('data-t',t)
+		
+		};
